b326af97f533609996c158ba3c02391de80e3e0d
Manuel Fernández Arjona <fernandez.arman20@triana.salesianos.edu>
2021-09-29 12:48:31 +0200

    Primer commit

---
+++ E04/src/main/java/triana/salesianos/dam/E04/E04CompartidoApplication.java
@@ -0,0 +1,13 @@
+package triana.salesianos.dam.E04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E04CompartidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E04CompartidoApplication.class, args);
+	}
+
+}
