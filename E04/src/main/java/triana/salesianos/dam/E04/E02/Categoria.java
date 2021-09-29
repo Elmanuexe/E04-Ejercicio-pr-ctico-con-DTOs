63cd568a3b79ef601335155b4c371372d54def8e
Pablo Repiso Sasiain <repiso.sapab21@triana.salesianos.edu>
2021-09-29 13:15:16 +0200

    Agrego las entidades y repositorios del subejercicio 02

---
+++ E04/src/main/java/triana/salesianos/dam/E04/E02/Categoria.java
@@ -0,0 +1,25 @@
+package triana.salesianos.dam.E04.E02;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Categoria {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String nombre;
+
+    public Categoria(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
---
+++ E04/src/main/java/triana/salesianos/dam/E04/E02/Producto.java
@@ -0,0 +1,32 @@
+package triana.salesianos.dam.E04.E02;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.List;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Producto {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String nombre;
+    private String desc;
+    private double pvp;
+    private List<String> imagenes;
+
+
+    @ManyToOne
+    private Categoria nombreCategoria;
+
+
+}
