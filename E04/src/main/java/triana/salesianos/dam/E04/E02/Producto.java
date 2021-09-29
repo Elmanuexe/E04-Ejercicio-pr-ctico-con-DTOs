a7a132c862828952063e51c0437aca8617ad6018
Pablo Repiso Sasiain <repiso.sapab21@triana.salesianos.edu>
2021-09-29 13:25:08 +0200

    @ElementCollection added

--- E04/src/main/java/triana/salesianos/dam/E04/E02/Producto.java
+++ E04/src/main/java/triana/salesianos/dam/E04/E02/Producto.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
@@ -22,6 +19,8 @@ public class Producto {
     private String nombre;
     private String desc;
     private double pvp;
+    
+    @ElementCollection
     private List<String> imagenes;
 
 
